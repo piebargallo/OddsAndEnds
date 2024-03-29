2a24f8f1129e27e0591f285cb9cb6f0fb796e11c
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-03-29 18:14:50 +0100

    Add files via upload

---
+++ exer_2/Simulator.cppm
@@ -0,0 +1,20 @@
+export module simulator;
+
+import <iostream>;
+using std::cout;
+using std::endl;
+
+export namespace Simulator
+{
+	class CarSimulator
+	{
+	public:
+		CarSimulator();
+	};
+
+	class BikeSimulator
+	{
+	public:
+		BikeSimulator();
+	};
+}
---
+++ exer_2/Test.cpp
@@ -0,0 +1,13 @@
+/* Take your solution from exer 11-1 and split the module into several files: a primary module interface file without
+   any implementations and two module implementation files, one for the CarSimulator and one for the BykeSimulation
+   class. */
+
+import simulator;
+
+int main()
+{
+	using namespace Simulator;
+
+	CarSimulator carSim;
+	BikeSimulator bikeSim;
+}
